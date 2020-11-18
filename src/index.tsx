4befb7fd7970ed6915795e55fa731a92be0e2c12
fujianjun <670189818@qq.com>
2020-11-18 17:08:58 +0800

    feat: add

--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,6 @@ import * as ReactDOM from 'react-dom'
 let root = document.getElementById('root')
 let props = {className: 'title'}
 let element = React.createElement('div', props, 'hello')
+
 ReactDOM.render(element, root)
 
